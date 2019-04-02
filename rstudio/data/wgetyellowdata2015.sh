11babf50b86efc45531c75817646003af530fa99
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2019-04-02 22:46:55 +0200

    new datasets for microsoft ml server exercises

---
+++ rstudio/data/wgetyellowdata2015.sh
@@ -0,0 +1,13 @@
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-01.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-02.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-03.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-04.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-05.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-06.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-07.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-08.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-09.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-10.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-11.csv
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2015-12.csv
+wget http://download2260.mediafire.com/eboubywk2odg/rzle0hm11onl101/yellow_tripdata_2015.xdf
