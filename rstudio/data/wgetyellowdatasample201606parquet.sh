9cb80964e91527b9294a5bd8b55d6096cca68a5c
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2019-04-28 16:25:14 +0200

    Spark parquet sample data and kernels to use with https://github.com/pwasiewi/jupyter-dockers

---
+++ rstudio/data/wgetyellowdatasample201606parquet.sh
@@ -0,0 +1,3 @@
+#wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2016-06.csv
+#echo Get the file in your browser: http://www.mediafire.com/file/q12fkyko059uies/yellow_tripdata_2016-06.xdf/file
+echo Get the parquet file in your browser: http://www.mediafire.com/file/1sea59b3ojp4i2x/yellow_tripdata_2016-06-parquet.tar.7z/file
